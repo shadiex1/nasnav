512e689b135bdbaffb7b00c2dd681623ebba89b9
shady ahmed <shadiex@hotmail.com>
2020-09-24 18:53:17 +0200

    Updates

---
+++ precache-manifest.07d812d7629a888c197bb00c58c3a24c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb005895f5d55ee47be353472c4451e",
+    "url": "/nasnav/index.html"
+  },
+  {
+    "revision": "b90a6a4c04906a5fda10",
+    "url": "/nasnav/static/css/2.2ee197a2.chunk.css"
+  },
+  {
+    "revision": "093a26aecb1f8a64ad51",
+    "url": "/nasnav/static/css/main.459354e8.chunk.css"
+  },
+  {
+    "revision": "b90a6a4c04906a5fda10",
+    "url": "/nasnav/static/js/2.f029959b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nasnav/static/js/2.f029959b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093a26aecb1f8a64ad51",
+    "url": "/nasnav/static/js/main.2455c3f7.chunk.js"
+  },
+  {
+    "revision": "ded35297d6c5ca1f12db",
+    "url": "/nasnav/static/js/runtime-main.093a8af0.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/nasnav/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/nasnav/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/nasnav/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/nasnav/static/media/slick.f97e3bbf.svg"
+  }
+]);
